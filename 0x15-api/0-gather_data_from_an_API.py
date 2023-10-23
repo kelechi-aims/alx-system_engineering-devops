33ba2eed4bb946e71f6e711d70235be3637be602
kelechi-aims <kelechi@aims.edu.gh>
2023-10-24 00:04:15 +0100

    READMe.md

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,22 +4,24 @@ Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
 
-    user_endpoint = '{}users/{}'.format(url, sys.argv[1])
+    user_endpoint = '{}users/{}'.format(url, argv[1])
     user_response = requests.get(user_endpoint)
     user_data = user_response.json()
     employee_name = user_data.get('name')
 
-    todos_endpoint = '{}todos?userID={}'.format(url, sys.argv[1])
-    todos_response = requests.get(todos_endpoint)
-    todos_data = todos_response.json()
+    todos_data = requests.get(url + "user/{}/todos".format(argv[1])).json()
 
-    completed_tasks = [task for task in todos_data if task.get('completed')]
+    completed_tasks = []
+    for task in todos_data:
+        if task.get('completed'):
+            completed_tasks.append(task)
+
     total_tasks = len(todos_data)
 
     completed_tasks_count = len(completed_tasks)
