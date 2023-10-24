ced93add51afcc2ed079804266101539c6baca3b
kelechi-aims <kelechi@aims.edu.gh>
2023-10-24 01:32:26 +0100

    task-2 corrections

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,16 +10,17 @@ import requests
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
 
-    user_endpoint = '{}users/{}'.format(url, argv[1])
+    user_id = argv[1]
+    user_endpoint = '{}users/{}'.format(url, user_id)
     user_response = requests.get(user_endpoint)
     user_data = user_response.json()
     employee_name = user_data.get('username')
 
-    todos_endpoint = '{}todos?userId={}'.format(url, argv[1])
+    todos_endpoint = '{}todos?userId={}'.format(url, user_id)
     todos_response = requests.get(todos_endpoint)
     todos_data = todos_response.json()
 
-    json_file_name = '{}.json'.format(argv[1])
+    json_file_name = '{}.json'.format(user_id)
     user_task = []
     for task in todos_data:
         task_data = {
@@ -29,7 +30,7 @@ if __name__ == '__main__':
         }
         user_task.append(task_data)
 
-    user_data_json = {str(argv[1]): user_task}
+    user_data_json = {str(user_id): user_task}
 
     with open(json_file_name, mode="w") as jsonfile:
         json.dump(user_data_json, jsonfile)
