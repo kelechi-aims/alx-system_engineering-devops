6fe2edc95f2b3250e53bd1a0f0d1a5bbb7d68a10
kelechi-aims <kelechi@aims.edu.gh>
2023-10-24 01:54:19 +0100

    task-

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that, using this REST API, gets information about TODO
+list progress for all employees and exports it to a JSON file.
+"""
+import requests
+import json
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    users_response = requests.get(url + 'users')
+    users_data = users_response.json()
+
+    all_tasks = {}
+
+    for user in users_data:
+        user_id = user.get('id')
+        username = user.get('username')
+
+        todos_response = requests.get(url + f'todos?userId={user_id}')
+        todos_data = todos_response.json()
+
+        user_tasks = []
+        for task in todos_data:
+            task_data = {
+                "username": username,
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            }
+            user_tasks.append(task_data)
+
+        all_tasks[str(user_id)] = user_tasks
+
+    json_file_name = 'todo_all_employees.json'
+
+    with open(json_file_name, 'w') as jsonfile:
+        json.dump(all_tasks, jsonfile)
