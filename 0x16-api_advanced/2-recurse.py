f2ed376f0ad17883753b2b856f8346d91eeacf8e
kelechi-aims <kelechi@aims.edu.gh>
2023-11-07 14:09:47 +0100

    task-2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,37 +1,39 @@
 #!/usr/bin/python3
-''' Recursive function that queries the Reddit API '''
+"""
+Recursive function that queries the Reddit API
+"""
 import requests
+import sys
+
+
+def append_title(hot_list, posts):
+    """ Appends item into a list """
+    if len(posts) == 0:
+        return
+    hot_list.append(posts[0]["data"]["title"])
+    posts.pop(0)
+    append_title(hot_list, posts)
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    '''
-    Returns a list containing the titles of all hot articles
-    for a given subreddit
-    '''
-    if after is None:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    else:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
-
-    # Set a custom User-Agent header as Reddit requires it
+    """ 
+    Returns a list containing the titles of all hot
+    articles for a given subreddit
+    """
     headers = {"User-Agent": "MyRedditBot"}
-    response = requests.get(url, headers=headers)
 
-    # Check if the subreddit exists and the request was successful
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-        # Extract titles and add them to the hot_list
-        for post in posts:
-            title = post["data"]["title"]
-            hot_list.append(title)
+    params = {"after": after}
 
-        # Check if there are more posts (pagination)
-        after = data["data"]["after"]
-        if after:
-            recurse(subreddit, hot_list, after)
-        else:
-            # No more posts, return the hot_list
-            return hot_list
-    else:
-        return None  # Return None for invalid subreddits or failed requests
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        return None
+
+    dic = response.json()
+    posts = dic["data"]["children"]
+    append_title(hot_list, posts)
+    after = dic["data"]["after"]
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
